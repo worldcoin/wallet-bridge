9810d63b44711d49a524483b50dd140ec66f7078
Miguel Piedrafita <github@miguel.build>
2023-09-15 07:27:14 +0100

    fix redis connection

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![warn(clippy::all, clippy::pedantic, clippy::nursery)]
 
 use dotenvy::dotenv;
-use redis::{aio::ConnectionManager, Client};
+use redis::aio::ConnectionManager;
 use std::env;
 
 mod routes;
@@ -18,12 +18,19 @@ async fn main() {
         .with_target(false)
         .init();
 
-    let redis = ConnectionManager::new(
-        Client::open(env::var("REDIS_URL").expect("REDIS_URL not set"))
-            .expect("Failed to create redis client"),
-    )
-    .await
-    .expect("Failed to create redis connection manager");
+    let redis = build_redis_pool(env::var("REDIS_URL").expect("REDIS_URL not set"))
+        .await
+        .expect("Failed to connect to Redis");
 
     server::start(redis).await;
+}
+
+async fn build_redis_pool(mut redis_url: String) -> redis::RedisResult<ConnectionManager> {
+    if !redis_url.starts_with("redis://") {
+        redis_url = format!("redis://{redis_url}");
+    }
+
+    let client = redis::Client::open(redis_url)?;
+
+    ConnectionManager::new(client).await
 }
