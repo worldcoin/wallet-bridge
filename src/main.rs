92ff17ed1c8a83750b6985a84ab5902e7441019a
Takis Kakalis <80459599+Takaros999@users.noreply.github.com>
2026-07-27 13:41:54 -0700

    chore: adopt telemetry batteries (#102)

--- src/main.rs
+++ src/main.rs
@@ -15,13 +15,7 @@ use utils::AppOverrides;
 async fn main() {
     dotenv().ok();
 
-    tracing_subscriber::fmt()
-        .with_max_level(tracing::Level::INFO)
-        .json()
-        .with_target(false)
-        .flatten_event(true)
-        .without_time()
-        .init();
+    let _telemetry_guard = telemetry_batteries::init().expect("Failed to initialize telemetry");
 
     tracing::info!("Starting wallet bridge...");
 
